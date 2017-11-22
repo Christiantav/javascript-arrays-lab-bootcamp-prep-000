591946cbf4bfe3b85916c3d66acb4b9438171dc7
Christian Tavares <chrisadrianotav@gmail.com>
2017-11-22 01:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(){
+  var cat = "Ralph"
+  kittens.push(cat);
+  return kittens;
+}
+function destructivelyPrependKitten(){
+  var cat = "Bob"
+  kittens.unshift(cat);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
